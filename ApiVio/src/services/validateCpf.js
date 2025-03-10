4ff3a037d6377b2f227fc976953b1b6d51f513c6
souzayasmin <ysmin.souza@hotmail.com>
2025-03-10 15:34:56 -0300

    Refatorando a userController 10/03

--- ApiVio/src/services/validateCpf.js
+++ ApiVio/src/services/validateCpf.js
@@ -1,26 +1,28 @@
 const connect = require("../db/connect");
 
-module.exports = async function validadeCpf(cpf, userId){
-   const query = "SELECT id_usuario FROM usuario WHERE cpf=?";
-   const values = [cpf];
+module.exports = async function validateCpf(cpf, userId = null) {
+  return new Promise((resolve, reject) => {
+    const query = "SELECT id_usuario FROM usuario WHERE cpf = ?";
+    const values = [cpf];
 
-   connect.query(query,values,(err,results)=>{
-    if (err){
-          //fazer algo
-    }
-    else if(results.length > 0){
-        const IdDoCpfCadastrado = results[0].id_usuario;
+    connect.query(query, values, (err, results) => {
+      if (err) {
+        reject("Erro ao verificar CPF");
+      } else if (results.length > 0) {
+        const cpfCadastrado = results[0].id_usuario;
 
-        if(userId && IdDoCpfCadastrado !== userId){
-            return { error:"CPF já cadastrado para outro usuário"}
-        }else if(!userId){
-            return{error: "CPF já cadastrado"}
+        // Se um userId foi passado (update) e o CPF pertence a outro usuário, retorna erro
+        if (userId && cpfCadastrado !== userId) {
+          resolve({ error: "CPF já cadastrado para outro usuário" });
+        } else if (!userId) {
+          resolve({ error: "CPF já cadastrado" });
+        } else {
+          resolve(null);
         }
-    }
-    else{
-        return null;
-    }
-   })
+      } else {
+        resolve(null);
+      }
+    });
+  });
+};
 
-
-}
