ca7f7680287d8940505d6fe848047869cbdda379
souzayasmin <ysmin.souza@hotmail.com>
2025-05-19 16:12:59 -0300

    implementação do docker 19/05

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,11 +1,12 @@
+// require('dotenv').config();
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
     connectionLimit:10,
-    host:'localhost',
-    user:'alunods',
-    password:'senai@604',
-    database:'vio_yasmin'
-})
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME
+});
 
 module.exports = pool;
