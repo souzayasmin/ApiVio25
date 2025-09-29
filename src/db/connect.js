efd8ea30c27990285ad06f59edf55932665bb1c2
souzayasmin <ysmin.souza@hotmail.com>
2025-09-29 13:18:05 -0300

    deploy 29/09

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,12 +1,13 @@
-require('dotenv').config();
-const mysql = require('mysql2');
+require("dotenv").config();
+const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-    connectionLimit:10,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-})
+  connectionLimit: 10,
+  host: process.env.MYSQLPORT || process.env.DB_HOST,
+  user: process.env.MYSQLPORT || process.env.DB_USER,
+  password: process.env.MYSQLPORT || process.env.DB_PASSWORD,
+  database: process.env.MYSQLPORT || process.env.DB_NAME,
+  port: process.env.MYSQLPORT || 3306,
+});
 
 module.exports = pool;
